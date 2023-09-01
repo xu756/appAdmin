7ec6545441308d3fa72dd05d60e15420496aa35a
xu756 <756334744@qq.com>
2023-09-01 15:49:51 +0800

    1

--- src/pages/mini/content/text.tsx
+++ src/pages/mini/content/text.tsx
@@ -1,3 +1,4 @@
+import Edit from '@/components/edit/edit';
 import { PathModel } from '@/components/path/pathModel';
 import Content from '@/models/content';
 import { PathState } from '@/models/path';
@@ -11,20 +12,24 @@ import {
   ProFormTextArea,
 } from '@ant-design/pro-components';
 import { history } from '@umijs/max';
-import { Image } from 'antd';
+import { Affix, Card, Image } from 'antd';
 import { useRef, useState } from 'react';
+import './content.scss';
 
 export default () => {
   const [content, setContent] = useState<Content>();
-  const formRef = useRef<ProFormInstance<Content>>();
+  const formRef = useRef<ProFormInstance>();
+
   const [ContentText, setContentText] = useState<string>('');
 
   const onFinish = async (values: Content) => {
     console.log(values);
   };
+
   return (
-    <>
+    <div className={'content'}>
       <ProForm<Content>
+        className={'content-from'}
         onFinish={onFinish}
         formRef={formRef}
         request={async () => {
@@ -60,22 +65,17 @@ export default () => {
               },
             ]}
           />
-            <ProFormText
-                width="lg"
-                name="title"
-                required
-                label="内容标题"
-                tooltip="最长为 24 位"
-                placeholder="请输入标题"
-                rules={[{ required: true, message: '这是必填项' }]}
-            />
+          <ProFormText
+            width="lg"
+            name="title"
+            required
+            label="内容标题"
+            tooltip="最长为 24 位"
+            placeholder="请输入标题"
+            rules={[{ required: true, message: '这是必填项' }]}
+          />
         </ProForm.Group>
-        <ProForm.Group>
 
-        </ProForm.Group>
-        <ProForm.Item label={"封面图片"} tooltip={"轮播图设置这个"}>
-          <Image width={'375px'} src={content?.img_url || ''} />
-        </ProForm.Item>
         <ProForm.Group>
           <ProFormTextArea
             width={'xl'}
@@ -83,15 +83,37 @@ export default () => {
             label="简介"
             placeholder="请输入简介"
           />
+          <ProForm.Item
+            label={'封面图片'}
+            tooltip={'轮播图设置这个'}
+            valueType={'image'}
+            name={'img_url'}
+          >
+            <Image width={'375px'} src={content?.img_url || ''} />
+          </ProForm.Item>
         </ProForm.Group>
-          <ProForm.Group >
-
-          </ProForm.Group>
+        <ProForm.Item label={'内容'} tooltip={'和小程序内部样式一样'}>
+          <Edit
+            html={ContentText}
+            onChange={(html) => {
+              setContentText(html);
+              formRef.current?.setFieldsValue({ content_text: html });
+            }}
+          />
+        </ProForm.Item>
       </ProForm>
+      <Affix className={'content-view'} offsetTop={120}>
+        <Card
+          title="小程序端内容效果"
+          bordered={false}
+        >
+            <div dangerouslySetInnerHTML={{ __html: ContentText }}></div>
+        </Card>
+      </Affix>
       <PathModel
         title={'找不到富文本内容'}
         open={history.location.state == undefined}
       />
-    </>
+    </div>
   );
 };
